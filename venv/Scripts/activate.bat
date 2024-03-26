1a2348175938dbf0c82f6b1c94d664f90277d08b
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2024-03-26 11:55:52 -0300

    Venv remake

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Repo\Image_to_pdf\venv
+set VIRTUAL_ENV=D:\MyRepo\imgToPdf\Image_to_pdf\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
